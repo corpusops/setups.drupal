a519e4fccf866e0f2d70d3cdb6e10d177b5b8381
regilero <regis.leroy@makina-corpus.com>
2016-06-24 18:11:40 +0200

    Drupal8 settings review

--- .salt/files/common.settings.php
+++ .salt/files/common.settings.php
@@ -4,9 +4,12 @@
 # {% set ddata = cfg.data %}
 # {% set data = ddata.local_settings %}
 
-$conf['reverse_proxy'] = {{data.reverse_proxy}};
-$conf['reverse_proxy_header'] = '{{data.reverse_proxy_header}}';
-$conf['reverse_proxy_addresses'] = {{data.reverse_proxy_addresses}};
+// D8 settings to "replace" $base_url configuration in D7 
+$settings['trusted_host_patterns'] = array('^{{ ddata.domain|replace(".", "\.") }}$'); 
+
+$settings['reverse_proxy'] = {{data.reverse_proxy}};
+$settings['reverse_proxy_header'] = '{{data.reverse_proxy_header}}';
+$settings['reverse_proxy_addresses'] = {{data.reverse_proxy_addresses}};
 
 /**
  * Vary Cookie: please Read
@@ -24,64 +27,38 @@ $conf['reverse_proxy_addresses'] = {{data.reverse_proxy_addresses}};
  * HTTP proxy, and bypass the reverse proxy if one is used) in order to avoid
  * getting cached pages from the proxy.
  */
-$conf['omit_vary_cookie'] = {{data.omit_vary_cookie}};
+$settings['omit_vary_cookie'] = {{data.omit_vary_cookie}};
 // Do not use the new image style token. it removes images from varnish and
 // breaks some modules
-$conf['image_allow_insecure_derivatives'] = {{ data.image_allow_insecure_derivatives }};
-
-// reduce uneeded queries by increasing the allowed max size of cached sentences (default is 75)
-$conf['locale_cache_length'] = {{ data.locale_cache_length }}; //204800;
+$config['image.settings']['allow_insecure_derivatives'] = {{ data.image_allow_insecure_derivatives }};
 // Imagecache
-$conf['image_jpeg_quality'] = {{ data.image_jpeg_quality}}; //95;
+$config['system.image.gd']['jpeg_quality'] = {{ data.image_jpeg_quality}}; //95;
 
 // CRON ///////////////////////////////////////////////////////////////
 // disallow the poor-man cron, we do it via drush
-$conf['cron_safe_threshold'] = 0;
+$config['automated_cron.settings']['interval'] = 0;
 // do not limit time for cron tasks
-$conf['elysia_cron_time_limit'] = 0;
+// FIXME: elysia_cron is a D only module for now...
+//$config['elysia_cron_time_limit'] = 0;
 
 {% if data.smtp.used %}
 // SMTP ///////////////////////////////////////////////////////////////
-$conf['smtp_on'] = 1;
-$conf['smtp_host'] = "{{ data.smtp.host }}";
-$conf['smtp_from'] = "{{ data.smtp.from }}";
-$conf['smtp_fromname'] = "{{ data.smtp.fromname }}";
+// FIXME: untested, check the smtp D8 module
+$config['system.mail']['smtp_on'] = TRUE;
+$config['system.mail']['smtp_host'] = "{{ data.smtp.host }}";
+$config['system.mail']['smtp_from'] = "{{ data.smtp.from }}";
+$config['system.mail']['smtp_fromname'] = "{{ data.smtp.fromname }}";
 {% endif %}
 
 // File system ////////////////////////////////////////////////////////////////
 // Warning: when PHP-FPM is chrooted, we musn't use real absolute path but only
 // "absolute path in the choot"
-$conf['file_directory_path'] = 'sites/default/files';
-$conf['file_public_path'] = $conf['file_directory_path'];
+$settings['file_public_path'] = 'sites/default/files';
 # www/sites/default/settings.php
 $root = "{{cfg.project_root}}";
-$conf['file_private_path'] = $root . '/var/private';
-$conf['file_temporary_path'] = $root . '/var/tmp';
-$conf['file_directory_temp'] = $conf['file_temporary_path'];
-
-// Cookies and cache //////////////////////////////////////////////////////////
-// NO_CACHE cookie on POST (can be used by nginx microcache and Varnish)
-// Enter the number of seconds to set a cookie for when a user submits any form
-// on the website. This will ensure that any page they see after submitting a
-// form will be dynamic and not cached.
-$conf['cookie_cache_bypass_adv_cache_lifetime']= {{ data.cookie_cache_bypass_adv_cache_lifetime }};
-// Choose whether or not the cookie will be set for just the path the user is
-// viewing when filling out the form, or it it will be set for the entire
-// website.
-// Please read the Cookie Cache Bypass Advanced module readme beforealtering
-// this setting
-$conf['cookie_cache_bypass_adv_cookie_path']='entire_site';
-// Choose when to set the cache bypass cookie. The safest time is before any
-// other validation scripts run, but this may cause people spamming your forms
-// to get more non-cached pages than you wish.
-// The least aggressive setting is as the last submit function.
-// However this setting may cause the cookie to not be set in some situations.
-// ['before_validate','before_submit','after_submit','after_validate']
-$conf['cookie_cache_bypass_adv_set_time']='after_validate';
-
-// FORM cache validity (default is 6 hours, several gigbytes of useless cache),
-// 1 hour should be enough
-$conf['form_cache_retention_time'] = 3600;
+$settings['file_private_path'] = $root . '/var/private';
+$config['system.file']['path']['temporary'] = $root . '/var/tmp';
+$config['system.file']['temporary_maximum_age'] = 21600; // in seconds 21600->6hours
 
 // File system permissions ////////////////////////////////////////////////////
 // default is 0775, we have user-group:www-data in sites/default/files
@@ -89,58 +66,39 @@ $conf['form_cache_retention_time'] = 3600;
 // user-group as the group of files in this directory, 'others' do
 // not need anything, so 2770 is good. But a 1st 0 should be added
 // to say it's an octal mode (and do not add quotes)
-$conf['file_chmod_directory']={{data.file_chmod_directory}};
+$settings['file_chmod_directory']={{data.file_chmod_directory}};
 // default is 0664
-$conf['file_chmod_file']={{data.file_chmod_file}};
+$settings['file_chmod_file']={{data.file_chmod_file}};
 
 // ensure nothing in the default multithread shared umask will break
 // our mkdir commands (chmod is not impacted, but mkdir is...)
 umask(0000);
 
-
 // default contact mail
-$conf['site_mail'] = '{{ data.site_email }}';
-
-// Pure performance: avoid banned ip query during bootstrap.
-$conf['blocked_ips'] = array();
+$config['system.site']['mail'] = '{{ data.site_email }}';
 
 // Log level //////////////////////////////////////////////////////////////////
-// 1 means some errors get to the end user, 2 means all errors, 0 none.
-$conf['error_level'] = {{ data.error_level }};
+// allowed values: 'hide', 'some', 'all', 'verbose'. 
+$config['system.logging']['error_level'] = '{{ data.error_level }}';
 
 // Compression ////////////////////////////////////////////////////////////////
+// Generate aggregates
+$config['system.performance']['css']['preprocess'] = {{ data.preprocess_css }};
+$config['system.performance']['js']['preprocess'] = {{ data.preprocess_js }};
+// Generate gzip version of aggregates
+$config['system.performance']['css']['preprocess'] = {{ data.css_gzip_compression }};
+$config['system.performance']['js']['preprocess'] = {{ data.js_gzip_compression }};
+
 // If compression is done on HTTPD level NEVER SET THIS to 1!
 $conf['page_compression'] = {{ data.page_compression }};
-// Generate aggregates
-$conf['preprocess_css'] = {{ data.preprocess_css }};
-$conf['preprocess_js'] =  {{ data.preprocess_js }};
-// Generate gzip version of aggregates
-$conf['js_gzip_compression'] = {{ data.js_gzip_compression }};
-$conf['css_gzip_compression'] = {{ data.css_gzip_compression }};
 
 // Define Drupal cache settings:--------------
-// this is the key of anonymous cached page headers generations
-$conf['cache'] = {{ data.cache }};
-$conf['block_cache'] = {{ data.cache_block }};
-// inactivate database connection if the cache backend doesn't need it (cache_page only)
-// if the page is not cached the db connection will be made later
-$conf['page_cache_without_database'] = FALSE;
-// avoid executing veryi early hooks in case of page cached (like hook_boot)
-$conf['page_cache_invoke_hooks']     = TRUE;
 // this will be in Cache-Control: public max-age
-$conf['page_cache_maximum_age'] = {{ data.page_cache_maximum_age }};
+$config['system.performance']['cache']['page']['max_age'] = {{ data.page_cache_maximum_age }};
 
-
-// This is the minimum cache validity
-//$conf['cache_lifetime'] = 10800; // 0 is infinite., 10800->6h
-// Broken feature (@Damien Tournoud http://drupal.org/node/1816424#comment-6730222)
-// Always put 0
-$conf['cache_lifetime'] = 0; // 0 is infinite., 10800->6h
-// Also bypass Drupal hardcoded block cache disabled when hook_node_grant
-// is implemented by at least one module
-// See and apply http://drupal.org/node/1930960#comment-7124130
-$conf['block_cache_bypass_node_grants'] = {{ data.block_cache_bypass_node_grants }}; // usually 1;
-
+// AVOID SENDING emails if not in production env -------------------------------
+$config['reroute_email.settings']['reroute_email_enable'] = {{ data.reroute_email_enable }};
+$config['reroute_email.settings']['reroute_email_address'] = '{{ data.reroute_email_address }}';
 
 $localsettingsfile = DRUPAL_ROOT . "/sites/default/local.settings.php";
 if (file_exists($localsettingsfile)) {
--- .salt/files/default.settings.php
+++ .salt/files/default.settings.php
@@ -4,6 +4,12 @@
 # {% set ddata = cfg.data %}
 # {% set data = ddata.local_settings %}
 
+// BASE URL --------------------------------------------------------------------
+$base_url = 'http://{{ ddata.domain }}';
+// this is a hack for drush in site-install mode, overriding base_url with crap,
+// at least you have a copy in variables, if needed
+$conf['base_url'] = $base_url;
+
 $databases = array();
 $databases['default']['default'] = array(
   'driver' => '{{ddata.db_type}}',
@@ -16,7 +22,7 @@ $databases['default']['default'] = array(
   'namespace' => '{{ddata.db_namespace}}',
 );
 
-$update_free_access = FALSE;
+$settings['update_free_access'] = FALSE;
 $drupal_hash_salt = '{{data.get(
     'drupal_hash',
     salt['mc_utils.generate_stored_password']('drupal_salt_'+cfg.name))}}';
@@ -30,11 +36,11 @@ ini_set('session.cookie_lifetime', 2000000);
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 
-$conf['404_fast_paths_exclude'] = '/\/(?:styles)\//';
-$conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
-$conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
+$config['system.performance']['fast_404']['exclude_paths'] = '/\/(?:styles)\//';
+$config['system.performance']['fast_404']['paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp|yml)$/i';
+$config['system.performance']['fast_404']['html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 
-$conf['allow_authorize_operations'] = FALSE;
+$settings['allow_authorize_operations'] = FALSE;
 
 // D8 config sync directory, better outside webroot than in a random directory in sites/default/files
 $config_directories = array(
@@ -44,9 +50,6 @@ $config_directories = array(
 );
 
 $settings['install_profile'] = '{{ddata.drupal_profile}}';
-
-// TODO: manage site uuid (can we?)
-//$config['system.site']['uuid'] ='ee71155f-2485-4d21-8604-4b842a37c850';
 
 $commonsettingsfile = DRUPAL_ROOT . "/sites/default/common.settings.php";
 if (file_exists($commonsettingsfile)) {
